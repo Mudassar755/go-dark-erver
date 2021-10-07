b1debd315a68fc5fcedc10d4dbc7755bacc5c8d9
Mudassar755 <mudasar.se@gmail>
2021-10-07 18:21:07 +0500

    resolving cors issue

--- index.js
+++ index.js
@@ -13,17 +13,34 @@
 const express = require('express')
 const auth = require("./api/auth")
 const mail = require("./api/mail")
+var useragent = require('express-useragent');
 const connectDB = require('./config/db');
-// var cors = require("cors");
-// var helmet = require('helmet')
+var cors = require("cors");
+var helmet = require('helmet')
 // const dotenv = require("dotenv");
 
 // dotenv.config();
 
 const app = express();
-// app.use(helmet())
+app.use(helmet())
 //Connect MongoDB
 connectDB();
+
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+    origin: function (origin, callback) {
+        // console.log("originn", origin)
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(useragent.express());
+app.use(helmet.hidePoweredBy())
+app.use(cors(corsOptions));
 app.use(express.json({extended: false}))
 
 app.use("/api/auth", auth)
