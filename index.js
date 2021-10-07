0c08ac70ff61bbf2cd22969b12efba4fe2e01eb9
Mudassar755 <mudasar.se@gmail>
2021-10-07 22:29:05 +0500

    email changes

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const product = require("./api/product");
-// const auth = require("./api/auth");
-
-// app.use(express.json({ extended: false }));
-
-// app.use("/api/product", product);
-// app.use("/api/auth", auth);
-
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 const express = require('express')
 const auth = require("./api/auth")
 const mail = require("./api/mail")
